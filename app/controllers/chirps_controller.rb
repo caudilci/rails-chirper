bd25915354687ceaf72f41f346321fd4172f77aa
Chris <christopheriancaudill@gmail.com>
2025-01-12 23:05:05 -0500

    Home

--- app/controllers/chirps_controller.rb
+++ app/controllers/chirps_controller.rb
@@ -2,4 +2,19 @@ class ChirpsController < ApplicationController
   def index
     @chirps = Chirp.all
   end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def delete
+  end
+
+  def destroy
+  end
 end
